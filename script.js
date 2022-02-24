ba3e0455a46cd63a01a2a82ec4035fd8e77eeb80
Valearexhepi <90195684+Valearexhepi@users.noreply.github.com>
2022-02-24 19:12:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function addCours() {
+    const input1 = document.querySelector("#input1");
+    const input2 = document.querySelector("#input2");
+    const input3 = document.querySelector("#input3");
+    const input4 = document.querySelector("#input4");
+    const input5 = document.querySelector("#input5");
+
+    input1.addEventListener("click", e => {
+
+    })
+    input1.addEventListener("click", e => {
+
+    })
+    input2.addEventListener("click", e => {
+
+    })
+    input3.addEventListener("click", e => {
+
+    })
+    input4.addEventListener("click", e => {
+
+    })
+
+
+    input5.innerHTML = input1.value;    
+
+}
+    
+    
+
